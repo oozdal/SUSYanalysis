5c5caf262fc291e7a7e9d5e8fbdada7423d09592
ozerozdal <ozerozdal@gmail.com>
2018-03-26 20:37:43 -0400

    Update

--- mainread.py
+++ mainread.py
@@ -1,29 +1,30 @@
-projectname = "examplemodel"
+projectname = "NonunigauginomchiBLR"
 
-#########################################################
+########################################################
 
 execfile("/Users/oozdal/readclass/packages.py")
-execfile("/Users/oozdal/readclass/functions.py")
+execfile("/Users/oozdal/projects/" +  projectname + "/filepaths.py")
 execfile("/Users/oozdal/ExpDATA/ExDATA.py")
 execfile("/Users/oozdal/readclass/classes.py")
 
 ########################################################
 
-examplemodel = createproject()
+SM = Standard_Model()
 
-########################################################
+#######################################################
 
-neutLHC=parameterset("neutralinolsp.dat")
-neutLHC.Create_mchi1_Content()
+#neutLHC=parameterset("neutralinolsp.dat")
+#neutLHC.Create_mchi1_Content()
 
-neutrelic=parameterset("neutralinorelic.dat")
-neutrelic.Create_mchi1_Content()
+#neutrelic=parameterset("neutralinorelic.dat")
+#neutrelic.Create_mchi1_Content()
 
 sneutrinoLHC=parameterset("sneutrinolsp.dat")
 sneutrinoLHC.CreateSv1Content()
 
 sneutrinorelic=parameterset("sneutrinorelic.dat")
 sneutrinorelic.CreateSv1Content()
-
+#sneutrinorelic.xSectionSignalStrength()
 ########################################################
 
+execfile("makegraph.py")
